19092530b4674aa1096e52fd2d3c0a5cc56e4a43
shengyanli1982 <shengyanlee36@gmail.com>
2024-01-29 12:50:28 +0800

    Optimize code structure and naming

--- group.go
+++ group.go
@@ -7,142 +7,143 @@ import (
 
 // 元素内存池
 // element pool.
-var elementPool = NewElementPool()
+var elementpool = NewElementPool()
 
 // 批量处理任务
 // batch process task.
 type Group struct {
-	items  []*Element     // 工作元素数组 (worker element array)
-	lock   sync.Mutex     // 锁 (lock)
-	config *Config        // 配置 (configuration)
-	wg     sync.WaitGroup // 等待组 (wait group)
-	once   sync.Once
-	ctx    context.Context
-	cancel context.CancelFunc
+	elements []*Element     // 工作元素数组 (worker element array)
+	lock     sync.Mutex     // 锁 (lock)
+	config   *Config        // 配置 (configuration)
+	wg       sync.WaitGroup // 等待组 (wait group)
+	once     sync.Once
+	ctx      context.Context
+	cancel   context.CancelFunc
 }
 
 // 创建一个新的批量处理任务
 // create a new batch process task.
 func NewGroup(conf *Config) *Group {
 	conf = isConfigValid(conf)
-	g := Group{
-		items:  []*Element{},
-		lock:   sync.Mutex{},
-		config: conf,
-		wg:     sync.WaitGroup{},
-		once:   sync.Once{},
+	gr := Group{
+		elements: []*Element{},
+		lock:     sync.Mutex{},
+		config:   conf,
+		wg:       sync.WaitGroup{},
+		once:     sync.Once{},
 	}
-	g.ctx, g.cancel = context.WithCancel(context.Background())
+	gr.ctx, gr.cancel = context.WithCancel(context.Background())
 
-	return &g
+	return &gr
 }
 
 // 停止批量处理任务
 // stop batch process task.
-func (g *Group) Stop() {
-	g.once.Do(func() {
-		g.cancel()
-		g.wg.Wait()
-		g.lock.Lock()
-		g.items = g.items[:0]
-		g.lock.Unlock()
+func (gr *Group) Stop() {
+	gr.once.Do(func() {
+		gr.cancel()
+		gr.wg.Wait()
+		gr.lock.Lock()
+		gr.elements = gr.elements[:0]
+		gr.lock.Unlock()
 	})
 }
 
 // 准备工作元素
 // prepare worker elements.
-func (g *Group) prepare(items []any) {
-	count := len(items)
-	g.lock.Lock()
+func (gr *Group) prepare(elements []any) {
+	count := len(elements)
+	gr.lock.Lock()
 
 	// 创建待处理的数据对象数组
 	// create data object array to be processed.
-	g.items = make([]*Element, count)
+	gr.elements = make([]*Element, count)
 
 	// 创建工作元素
 	// create worker elements.
 	for i := 0; i < count; i++ {
 		// 从内存池中获取一个元素
 		// get an element from element pool.
-		e := elementPool.Get()
+		element := elementpool.Get()
 
 		// 设置数据和值
 		// set data and value.
-		e.SetData(items[i])
-		e.SetValue(int64(i))
+		element.SetData(elements[i])
+		element.SetValue(int64(i))
 
 		// 将元素放入工作元素数组
 		// put element into worker element array.
-		g.items[i] = e
+		gr.elements[i] = element
 	}
-	g.lock.Unlock()
+
+	gr.lock.Unlock()
 }
 
 // 执行批量处理任务
 // execute batch process task.
-func (g *Group) execute() []any {
+func (gr *Group) execute() []any {
 	var results []any
 	// 如果需要返回结果, 则创建结果数组
 	// if need return result, create result array.
-	if g.config.result {
-		results = make([]any, len(g.items))
+	if gr.config.result {
+		results = make([]any, len(gr.elements))
 	}
 
 	// 启动工作者
 	// start workers.
-	g.wg.Add(g.config.num)
-	for i := 0; i < g.config.num; i++ {
+	gr.wg.Add(gr.config.num)
+	for i := 0; i < gr.config.num; i++ {
 		go func() {
-			defer g.wg.Done()
+			defer gr.wg.Done()
 			for {
 				select {
-				case <-g.ctx.Done():
+				case <-gr.ctx.Done():
 					return
 
 				default:
-					g.lock.Lock()
+					gr.lock.Lock()
 					// 如果没有元素, 则返回
 					// if no element, return.
-					if len(g.items) == 0 {
-						g.lock.Unlock()
+					if len(gr.elements) == 0 {
+						gr.lock.Unlock()
 						return
 					}
 
 					// 从工作元素数组中获取一个元素
 					// get an element from worker element array.
-					item := g.items[0]
+					element := gr.elements[0]
 
 					// 从工作元素数组中移除一个元素，并将第一个元素清理
 					// remove an element from worker element array and clean the first element.
-					g.items[0] = nil
-					g.items = g.items[1:]
-					g.lock.Unlock()
+					gr.elements[0] = nil
+					gr.elements = gr.elements[1:]
+					gr.lock.Unlock()
 
 					// 获取数据
 					// get data.
-					d := item.GetData()
+					data := element.GetData()
 
 					// 执行回调函数 OnBefore
 					// execute callback function OnBefore.
-					g.config.callback.OnBefore(d)
+					gr.config.callback.OnBefore(data)
 
 					// 执行消息处理函数
 					// execute message handle function.
-					r, err := g.config.handleFunc(d)
+					result, err := gr.config.handleFunc(data)
 
 					// 执行回调函数 OnAfter
 					// execute callback function OnAfter.
-					g.config.callback.OnAfter(d, r, err)
+					gr.config.callback.OnAfter(data, result, err)
 
 					// 如果需要返回结果, 则将结果放入结果数组
 					// if need return result, put result into result array.
-					if g.config.result {
-						results[item.GetValue()] = r
+					if gr.config.result {
+						results[element.GetValue()] = result
 					}
 
 					// 将元素放入内存池
 					// put element into element pool.
-					elementPool.Put(item)
+					elementpool.Put(element)
 				}
 			}
 		}()
@@ -150,25 +151,27 @@ func (g *Group) execute() []any {
 
 	// 等待工作者完成
 	// wait workers done.
-	g.wg.Wait()
+	gr.wg.Wait()
 
+	// 返回结果数组
+	// return result array.
 	return results
 }
 
 // 批量处理
 // batch process.
-func (g *Group) Map(items []any) []any {
+func (gr *Group) Map(elements []any) []any {
 	// 如果没有元素, 直接返回
 	// if no element, return directly.
-	if len(items) == 0 {
+	if len(elements) == 0 {
 		return nil
 	}
 
 	// 准备工作元素
 	// prepare worker elements.
-	g.prepare(items)
+	gr.prepare(elements)
 
 	// 执行批量处理任务
 	// execute batch process task.
-	return g.execute()
+	return gr.execute()
 }
